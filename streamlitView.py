ede0fad77e761a29a80c019552f25da294b108e8
AsutoshDalei <asutoshdalei@gmail.com>
2024-12-14 19:58:30 -0500

    sidebar section development

--- streamlitView.py
+++ streamlitView.py
@@ -3,13 +3,10 @@ import matplotlib.pyplot as plt
 # import torch
 import warnings
 warnings.filterwarnings("ignore")
-
 import streamlit as st
 
-
-st.title("SurveilAI Platform")
-st.markdown("Empowering Security with :red[Real-time Object Detection] and :blue[Intelligent Surveillance].")
-st.divider()
+# Extras
+RTSPDEFINATION = 'RTSP (Real-Time Streaming Protocol) is used for streaming media between devices, enabling real-time control over playback.'
 
 @st.dialog("Warning: Responsible Use of SurveilAI")
 def agreement():
@@ -29,31 +26,66 @@ By using this platform, you acknowledge:
     else:
         st.session_state.agreement = {'status':False,'name':userName,'email':userEmail}
 
+# Session States
 if "agreement" not in st.session_state:
     agreement()
 
 if "userState" not in st.session_state:
     st.session_state.userState = {'userName':None,'numFeeds':0,'config':False}
 
+if "disableConfig" not in st.session_state:
+    st.session_state.disableConfig = False
 
+def disableFnx(status):
+    st.session_state.disableConfig = status
+
+
+# Sidebar Section
 with st.sidebar:
     st.title("SurveilAI Stats Platform.")
     with st.expander("System Configuration"):
-        st.session_state.userState['userName'] = st.text_input(label="User Name",value=None,help='Kindly provide your name.') 
-        st.session_state.userState['numFeeds'] = st.slider(label="Number of Camera Feeds (RTSP needed)",min_value=0,max_value=3,step=1,help='RTSP (Real-Time Streaming Protocol) is used for streaming media between devices, enabling real-time control over playback.')
-
+        st.session_state.userState['userName'] = st.text_input(label="User Name",value=None,help='Kindly provide your name.', disabled=st.session_state.disableConfig) 
+        # st.session_state.userState['numFeeds'] = st.slider(label="Number of Camera Feeds (RTSP needed)",min_value=0,max_value=3,step=1,help='RTSP (Real-Time Streaming Protocol) is used for streaming media between devices, enabling real-time control over playback.')
+        st.session_state.userState['numFeeds'] = st.segmented_control(label="Number of Camera Feeds (RTSP needed)",options=[0,1,2,3], default=0,help=RTSPDEFINATION, disabled=st.session_state.disableConfig)
+   
     for feedPtr in range(st.session_state.userState['numFeeds']):
         st.session_state[f'feed{feedPtr+1}'] = {'name':'','link':None}
         with st.expander(f"Feed{feedPtr+1} Configuration"):
             st.session_state[f'feed{feedPtr+1}']['name'] = st.text_input("Feed Name:",f'Feed{feedPtr+1}',key=f'feed{feedPtr+1}Name')
-            st.session_state[f'feed{feedPtr+1}']['link'] = st.text_input("RTSP Link:",None,key=f'feed{feedPtr+1}Link')
-    
-    st.divider()
-    # st.write(st.session_state.temp)
+            st.session_state[f'feed{feedPtr+1}']['link'] = st.text_input("RTSP Link:",None,key=f'feed{feedPtr+1}Link', disabled=st.session_state.disableConfig)
 
-# st.write([st.session_state[f'feed{feedPtr+1}']['name']] for feedPtr in range(st.session_state.userState['numFeeds']))
-st.write([st.session_state[f'feed{feedPtr+1}']['name']] for feedPtr in range(st.session_state.userState['numFeeds']))
-# tabs = st.tabs([st.session_state[f'feed{feedPtr+1}']['name']] for feedPtr in range(st.session_state.userState['numFeeds']))
+    st.divider()
+    st.divider()
+    sideBarcol1, sideBarcol2 = st.columns(2)
+    with sideBarcol1:
+        if st.button("Submit Setup", help="Click to submit configurations."):
+            disableFnx(True)
+            st.success('Setup', icon="✅")
+            
+
+    with sideBarcol2:
+        if st.button("Reset Setup", type = 'primary', help='Click to reset configurations.'):
+            disableFnx(False)
+            st.rerun()
+
+# Primary Section
+st.title("SurveilAI Platform")
+st.markdown("Empowering Security with :red[Real-time Object Detection] and :blue[Intelligent Surveillance].")
+
+if st.session_state.userState['userName'] != None:
+    # st.caption(f"Hello {st.session_state.userState['userName']}")
+    st.markdown(f"*Hello {st.session_state.userState['userName']}*")
+st.divider()
+
+
+if st.session_state.userState['numFeeds'] > 0:
+    tabNames = []
+    for feedPtr in range(st.session_state.userState['numFeeds']):
+        tabNames.append(st.session_state[f'feed{feedPtr+1}']['name'])
+    tabs = st.tabs(tabNames)
+
+else:
+    st.info('System Configuration Needed.', icon="ℹ️")
 
 
 
