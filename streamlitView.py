b7aa95ce5b4df2223b3cf3777516b00857610ed6
AsutoshDalei <asutoshdalei@gmail.com>
2024-12-22 11:49:15 -0500

    multifeed work in progress

--- streamlitView.py
+++ streamlitView.py
@@ -6,6 +6,7 @@ warnings.filterwarnings("ignore")
 import streamlit as st
 
 # FEED: rtsp://NELTJRSl:8z3Y969kgO6sGTpX@192.168.1.216:554/live/ch0
+# FEED2: rtsp://NELTJRSl:8z3Y969kgO6sGTpX@192.168.1.216:554/live/ch0
 
 # Vision Functions
 ## Need to make this of multi feeds
@@ -36,20 +37,28 @@ def cameraAccessCCTV(feed,feedname, stElement):
 
 def cameraAccessMultiFeed(feeds,feednames,stElems,elemNums):
     feedMap,stMap,stButton,frameHolder = {},{},{},{}
-    for feed,stElem,elemNum in zip(feeds,stElems,elemNums):
+    for feed,stElem,elemNum in zip(feeds,stElems,range(elemNums)):
         feedMap[f'feed{elemNum}'] = cv.VideoCapture(feed)
+        print(elemNum)
         if not feedMap[f'feed{elemNum}'].isOpened():
             stMap[f'feed{elemNum}'].error("Camera Access Unavailable", icon="🚨")
         else:
-            stButton[f'feed{elemNum}'] = stElem.button("Stop Feed",help='Click to stop the feed.')
+            stButton[f'feed{elemNum}'] = stElem.button("Stop Feed",help='Click to stop the feed.',key=f'feed{elemNum}')
             frameHolder[f'feed{elemNum}'] = stElem.empty()
   
     while resetButton != True:
-        for elemNum in elemNums:
+        for elemNum in range(elemNums):
             if feedMap[f'feed{elemNum}'].isOpened() and not stButton[f'feed{elemNum}']:
                 ret, frame = feedMap[f'feed{elemNum}'].read()
+                if not ret:
+                    stElem[f'feed{elemNum}'].error("Frame Unavailable.", icon="🚨")
+                    break
+                gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+                frameHolder[f'feed{elemNum}'].image(frame)
 
-            
+    for elemNum in range(elemNums):
+        feedMap[f'feed{elemNum}'].release()
+
 
 
 # Extras
@@ -124,17 +133,19 @@ st.markdown("Empowering Security with :red[Real-time Object Detection] and :blue
 st.divider()
 
 if (submitButton == True) and (st.session_state.userState['numFeeds'] > 0):
-    tabNames = []
-    for feedPtr in range(st.session_state.userState['numFeeds']):
-        tabNames.append(st.session_state[f'feed{feedPtr+1}']['name'])
+    tabNames = [st.session_state[f'feed{feedPtr+1}']['name'] for feedPtr in range(st.session_state.userState['numFeeds'])]
+    feedLinks = [st.session_state[f'feed{feedPtr+1}']['link'] for feedPtr in range(st.session_state.userState['numFeeds'])]
+    # for feedPtr in range(st.session_state.userState['numFeeds']):
+    #     tabNames.append(st.session_state[f'feed{feedPtr+1}']['name'])
     tabs = st.tabs(tabNames)
 
-    for feedTab,nameTab,feedPtr in zip(tabs,tabNames,range(st.session_state.userState['numFeeds'])):
-        feedTab.subheader(nameTab)
-        feedLink = st.session_state[f'feed{feedPtr+1}']['link']
+    # for feedTab,nameTab,feedPtr in zip(tabs,tabNames,range(st.session_state.userState['numFeeds'])):
+    #     feedTab.subheader(nameTab)
+    #     feedLink = st.session_state[f'feed{feedPtr+1}']['link']
 
-        cameraAccessCCTV(feed=feedLink, feedname=nameTab, stElement=feedTab)
+    #     # cameraAccessCCTV(feed=feedLink, feedname=nameTab, stElement=feedTab)
 
+    cameraAccessMultiFeed(feeds=feedLinks,feednames=tabNames,stElems=tabs,elemNums=st.session_state.userState['numFeeds'])
 
 else:
     st.info('System Configuration Needed.', icon="ℹ️")
